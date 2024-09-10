e44dc2068567fb41945fcf2c17e849ca409a4813
[miccx-24] <[macdonaldsairos24@gmail.com]>
2024-09-10 12:29:13 +0200

    our 2nd commit

---
+++ miccs_joyce/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar'
+import Home from './Home'
+const App =()=> {
+    return (
+        <div>
+            <Navbar/>
+            <Home/>
+        </div>
+        
+    )                                                                                          
+}
+export default App;
---
+++ miccs_joyce/src/Home.js
@@ -0,0 +1,9 @@
+const Home =()=> {
+    return (
+        <div>
+            <h2>Our first react app</h2>
+        </div>
+        
+    )                                                                                          
+}
+export default Home;
